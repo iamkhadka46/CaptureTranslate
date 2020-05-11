f6ce94ea3438f1ecbb87ac5582e786a5c4e7099f
iamkhadka46 <58580576+iamkhadka46@users.noreply.github.com>
2020-05-11 17:46:58 +0545

    UI changes and bug fixes!

--- src/main/java/com/example/capturetranslate/MainActivity.java
+++ src/main/java/com/example/capturetranslate/MainActivity.java
@@ -2,14 +2,18 @@ package com.example.capturetranslate;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.FileProvider;
 
 import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.graphics.Matrix;
 import android.graphics.drawable.BitmapDrawable;
+import android.media.ExifInterface;
 import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
 import android.provider.MediaStore;
 import android.text.method.ScrollingMovementMethod;
 import android.view.View;
@@ -25,10 +29,14 @@ import com.google.firebase.ml.vision.common.FirebaseVisionImage;
 import com.google.firebase.ml.vision.text.FirebaseVisionText;
 import com.google.firebase.ml.vision.text.FirebaseVisionTextRecognizer;
 
+import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.List;
+import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -38,7 +46,8 @@ public class MainActivity extends AppCompatActivity {
     private ImageView imageView;
 
     private Bitmap imageBitmap, galleryBitmap, camera, gallery;
-    private BitmapDrawable frmCamera, frmGallery;
+    private BitmapDrawable frmCamera, frmGallery, frmScreen;
+    String cameraFilePath;
 
     static final int REQUEST_IMAGE_CAPTURE = 1;
 
@@ -78,6 +87,7 @@ public class MainActivity extends AppCompatActivity {
 
             @Override
             public void onClick(View v) {
+                try{
                 if (imageView.getDrawable()!= null){
                 BitmapDrawable check = (BitmapDrawable) imageView.getDrawable();
 
@@ -86,31 +96,83 @@ public class MainActivity extends AppCompatActivity {
 
                 }
                 else if(check == frmCamera){
-                    detectTextFromCamera();
-                }
+                    try {
+                        detectTextFromCamera();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }}
                 else{
-                    Toast.makeText(MainActivity.this, "No Text :(", Toast.LENGTH_LONG).show();
+                    Toast.makeText(MainActivity.this, "No Image Uploaded!", Toast.LENGTH_LONG).show();
                 }
-                Intent activity2Intent = new Intent(getApplicationContext(), TranslateActivity.class);
-                startActivity(activity2Intent);
-            }}
-        });
+                    Intent activity2Intent = new Intent(getApplicationContext(), TranslateActivity.class);
+                    startActivity(activity2Intent);
+            } catch (ClassCastException e){
 
+                }
+        }} );
+    }
+        //frmScreen = (BitmapDrawable) imageView.getDrawable();
+
+
+
+    private File createImageFile() throws IOException {
+        // Create an image file name
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+        String imageFileName = "JPEG_" + timeStamp + "_";
+        File storageDir = getExternalFilesDir(Environment.DIRECTORY_PICTURES);
+        File image = File.createTempFile(
+                imageFileName,  /* prefix */
+                ".jpg",         /* suffix */
+                storageDir      /* directory */
+        );
+
+        // Save a file: path for use with ACTION_VIEW intents
+        cameraFilePath = image.getAbsolutePath();
+        return image;
     }
 
+
     private void chooseImage() {
-        Intent photoPickerIntent = new Intent(Intent.ACTION_PICK);
+        Intent photoPickerIntent = new Intent(Intent.ACTION_GET_CONTENT);
         photoPickerIntent.setType("image/*");
         startActivityForResult(photoPickerIntent, 0);
     }
 
 
     private void dispatchTakePictureIntent() {
+        /*try {
+            Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+            intent.putExtra(MediaStore.EXTRA_OUTPUT, FileProvider.getUriForFile(this,
+                    BuildConfig.APPLICATION_ID + ".provider", createImageFile()));
+            startActivityForResult(intent, 1);
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }*/
+
         Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        // Ensure that there's a camera activity to handle the intent
         if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
-            startActivityForResult(takePictureIntent, 1);
+            // Create the File where the photo should go
+            File photoFile = null;
+            try {
+                photoFile = createImageFile();
+            } catch (IOException ex) {
+                // Error occurred while creating the File
+            }
+            // Continue only if the File was successfully created
+            if (photoFile != null) {
+                Uri photoURI = FileProvider.getUriForFile(this,
+                        "com.example.android.fileprovider",
+                        photoFile);
+                takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI);
+                startActivityForResult(takePictureIntent, 1);
+            }
         }
+
     }
+
+
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
@@ -130,21 +192,51 @@ public class MainActivity extends AppCompatActivity {
                 }}// Do your stuff here...
                 break;
             case 1:
-                if(data != null){
-                Bundle extras = data.getExtras();
-                imageBitmap = (Bitmap) extras.get( "data" );
-                imageView.setImageBitmap( imageBitmap );
-                frmCamera = (BitmapDrawable) imageView.getDrawable();// Do your other stuff here...
+                File file = new File(cameraFilePath);
+                try {
+                    imageBitmap =  MediaStore.Images.Media.getBitmap(this.getContentResolver(),Uri.fromFile(file));
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+                if (imageBitmap !=null){
+                    imageView.setImageBitmap( rotateImage(imageBitmap) );
+                    frmCamera = (BitmapDrawable) imageView.getDrawable();
+                }
                 break;
             }
             ///case etc:
                 //break;
         }
+
+    private Bitmap rotateImage(Bitmap bitmap) {
+        ExifInterface exifInterface = null;
+        try {
+            exifInterface = new ExifInterface( cameraFilePath );
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        int orientation = exifInterface.getAttributeInt( ExifInterface.TAG_ORIENTATION, ExifInterface.ORIENTATION_UNDEFINED );
+        Matrix matrix = new Matrix();
+        switch (orientation) {
+            case ExifInterface.ORIENTATION_ROTATE_90:
+                matrix.setRotate( 90 );
+                break;
+
+            case ExifInterface.ORIENTATION_ROTATE_180:
+                matrix.setRotate( 180 );
+                break;
+
+            default:
+        }
+        Bitmap rotatedBitmap = Bitmap.createBitmap( bitmap, 0, 0, bitmap.getWidth(), bitmap.getHeight(), matrix, true );
+        return rotatedBitmap;
+
     }
 
-    private void detectTextFromCamera(){
+    private void detectTextFromCamera() throws IOException {
 
-        FirebaseVisionImage image = FirebaseVisionImage.fromBitmap(imageBitmap);
+        FirebaseVisionImage image = FirebaseVisionImage.fromBitmap( imageBitmap );
         FirebaseVisionTextRecognizer detector = FirebaseVision.getInstance().getOnDeviceTextRecognizer();
         detector.processImage(image).addOnSuccessListener(new OnSuccessListener<FirebaseVisionText>() {
             @Override
@@ -185,7 +277,7 @@ public class MainActivity extends AppCompatActivity {
                     } else {
                         for (FirebaseVisionText.TextBlock block : firebaseVisionText.getTextBlocks()) {
                             String txt = block.getText();
-                            TranslateActivity.mSourceText.setTextSize(20);
+                            //TranslateActivity.mSourceText.setTextSize(20);
                             TranslateActivity.mSourceText.setText(txt);
 
                         }
--- src/main/java/com/example/capturetranslate/TranslateActivity.java
+++ src/main/java/com/example/capturetranslate/TranslateActivity.java
@@ -1,13 +1,18 @@
 package com.example.capturetranslate;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.graphics.Paint;
 import android.os.Bundle;
+import android.speech.RecognizerIntent;
+import android.speech.tts.TextToSpeech;
 import android.text.method.ScrollingMovementMethod;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageButton;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -19,13 +24,18 @@ import com.google.firebase.ml.naturallanguage.translate.FirebaseTranslateLanguag
 import com.google.firebase.ml.naturallanguage.translate.FirebaseTranslator;
 import com.google.firebase.ml.naturallanguage.translate.FirebaseTranslatorOptions;
 
+import java.util.ArrayList;
+import java.util.Locale;
+
 public class TranslateActivity extends AppCompatActivity {
 
     public static EditText mSourceText;
     private TextView mSourceLang;
     private Button mTranslateBtn;
+    private ImageButton addVoice, listen, stop, share, shareTop;
     private TextView mTranslatedText;
     private String sourceText;
+    TextToSpeech mTTS;
 
 
     @Override
@@ -35,10 +45,79 @@ public class TranslateActivity extends AppCompatActivity {
         mSourceLang = findViewById( R.id.sourceLang );
         mSourceText = findViewById( R.id.sourceText );
         mTranslateBtn = findViewById( R.id.translate );
+        addVoice = findViewById( R.id.voice );
+        listen = findViewById( R.id.listen );
+        stop = findViewById( R.id.stop );
+        share = findViewById( R.id.share );
+        shareTop = findViewById( R.id.shareTop );
         mTranslatedText = findViewById( R.id.translatedText );
-        mSourceText.setMovementMethod(new ScrollingMovementMethod());
-        mTranslatedText.setMovementMethod(new ScrollingMovementMethod());
-        
+        mSourceText.setMovementMethod( new ScrollingMovementMethod() );
+        mTranslatedText.setMovementMethod( new ScrollingMovementMethod() );
+
+
+        mTTS = new TextToSpeech( getApplicationContext(), new TextToSpeech.OnInitListener() {
+            @Override
+            public void onInit(int status) {
+                if (status != TextToSpeech.ERROR) {
+                    mTTS.setLanguage( Locale.US );
+                } else {
+                    Toast.makeText( TranslateActivity.this, "Error", Toast.LENGTH_SHORT ).show();
+                }
+
+            }
+        } );
+
+        listen.setOnClickListener( new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String toSpeak = mTranslatedText.getText().toString().trim();
+                if (toSpeak.equals( "" )) {
+                    Toast.makeText( TranslateActivity.this, "No translated text!", Toast.LENGTH_SHORT ).show();
+                } else {
+                    Toast.makeText( TranslateActivity.this, toSpeak, Toast.LENGTH_SHORT ).show();
+                    //speak the text
+                    mTTS.speak( toSpeak, TextToSpeech.QUEUE_FLUSH, null );
+                }
+            }
+        } );
+
+        stop.setOnClickListener( new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (mTTS.isSpeaking()) {
+                    mTTS.stop();
+
+                } else {
+                    Toast.makeText( TranslateActivity.this, "Not Speaking!", Toast.LENGTH_SHORT ).show();
+                }
+            }
+        } );
+
+        share.setOnClickListener( new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String text = mTranslatedText.getText().toString();
+                Intent sharingIntent = new Intent( Intent.ACTION_SEND);
+                sharingIntent.setType( "text/plain" );
+                sharingIntent.putExtra( Intent.EXTRA_SUBJECT, "Subject Here" );
+                sharingIntent.putExtra(Intent.EXTRA_TEXT, text);
+                startActivity( Intent.createChooser( sharingIntent, "Share translated text via" ) );
+            }
+        } );
+
+        shareTop.setOnClickListener( new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String text = mSourceText.getText().toString();
+                Intent sharingIntent = new Intent( Intent.ACTION_SEND);
+                sharingIntent.setType( "text/plain" );
+                sharingIntent.putExtra( Intent.EXTRA_SUBJECT, "Subject Here" );
+                sharingIntent.putExtra(Intent.EXTRA_TEXT, text);
+                startActivity( Intent.createChooser( sharingIntent, "Share translated text via" ) );
+            }
+        } );
+
+
         mTranslateBtn.setOnClickListener( new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -46,6 +125,35 @@ public class TranslateActivity extends AppCompatActivity {
             }
         } );
 
+        addVoice.setOnClickListener( new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent voiceIntent = new Intent( RecognizerIntent.ACTION_RECOGNIZE_SPEECH );
+                voiceIntent.putExtra( RecognizerIntent.EXTRA_LANGUAGE_MODEL, RecognizerIntent.LANGUAGE_MODEL_FREE_FORM );
+                voiceIntent.putExtra( RecognizerIntent.EXTRA_PROMPT, "Speech to Text" );
+                startActivityForResult( voiceIntent, 1 );
+            }
+        } );
+    }
+
+
+
+    @Override
+    protected void onPause() {
+        if (mTTS != null || mTTS.isSpeaking()) {
+            mTTS.stop();
+        super.onPause();
+    }}
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult( requestCode, resultCode, data );
+        if(requestCode == 1 && resultCode == RESULT_OK){
+            ArrayList<String> matches = data.getStringArrayListExtra( RecognizerIntent.EXTRA_RESULTS );
+            mSourceText.setText( matches.get(0).toString() );
+
+
+        }
     }
 
     private void identifyLanguage() {
@@ -111,7 +219,7 @@ public class TranslateActivity extends AppCompatActivity {
                 translator.translate( sourceText ).addOnSuccessListener( new OnSuccessListener<String>() {
                     @Override
                     public void onSuccess(String s) {
-                        mTranslatedText.setTextSize( 20 );
+                        //mTranslatedText.setTextSize( 20 );
                         mTranslatedText.setText( s );
                     }
                 } );
